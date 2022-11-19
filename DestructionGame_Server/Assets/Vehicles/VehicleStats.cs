c30d33840683939df4d15a9c3995c2d9e484991a
Liam Shelton <liam.shelton@snhu.edu>
2022-11-19 15:30:09 -0500

    Vehicle Model
    Including all iterations as proof. spent~3 hours on it by now

---
+++ DestructionGame_Server/Assets/Vehicles/VehicleStats.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleStats : MonoBehaviour
+{
+    public float health = 50f;
+    public float forceMultiplier = 1f;
+
+    public string DeathFXPrefab;
+
+    public virtual void Update()
+    {
+
+    }
+
+    void MonitorHealth()
+    {
+
+    }
+}
